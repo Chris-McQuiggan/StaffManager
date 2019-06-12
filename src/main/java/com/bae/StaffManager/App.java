e7288b148706083248742d00f1246ed8f578d7c4
Chris McQuiggan <chris@mcquiggan.info>
2019-06-12 08:48:46 +0100

    first

---
+++ src/main/java/com/bae/StaffManager/App.java
@@ -0,0 +1,13 @@
+package com.bae.StaffManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
